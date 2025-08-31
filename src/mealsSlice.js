710ffcb789c404958bbb708c934e56477ccdb6e8
surya-dev-101 <suryapersonal101@gmail.com>
2025-08-31 16:34:33 +0530

    practice lab exercise

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,15 +1,20 @@
 // mealsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const mealsSlice = createSlice({
-  name: 'meals',
-  initialState: [
-   
-  ],
-  reducers: {
-    toggleMealSelection: (state, action) => {
-    },
-  },
+	name: "meals",
+	initialState: [
+		{ name: "Breakfast", cost: 50, selected: false },
+		{ name: "High Tea", cost: 25, selected: false },
+		{ name: "Lunch", cost: 65, selected: false },
+		{ name: "Dinner", cost: 70, selected: false },
+	],
+	reducers: {
+		toggleMealSelection: (state, action) => {
+            const item = state[action.payload];
+            if (item) item.selected = !item.selected;
+        },
+	},
 });
 
 export const { toggleMealSelection } = mealsSlice.actions;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import venueReducer from "./venueSlice";
+import avReducer from "./avSlice";
+import mealReducer from "./mealsSlice";
 
 export default configureStore({
-  reducer: {
-    venue: venueReducer,
-  },
+	reducer: {
+		venue: venueReducer,
+		av: avReducer,
+		meals: mealReducer,
+	},
 });
